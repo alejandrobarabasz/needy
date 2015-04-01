71bbd573affffecab505c95cbab4b62c0a5800fe
Alejandro Barabasz <alejandro.barabasz@elserver.com>
2015-04-01 00:34:11 -0300

    Added migration to create support_centers table

--- database/migrations/2015_03_28_012105_create_support_centers_table.php
+++ database/migrations/2015_03_28_012105_create_support_centers_table.php
@@ -14,8 +14,23 @@ class CreateSupportCentersTable extends Migration {
 	{
 		Schema::create('support_centers', function(Blueprint $table)
 		{
-			$table->increments('id');
+			// Columns
+			$table->bigIncrements('id');
+			$table->unsignedBigInteger('account_id');
+			$table->string('name', 255);
+			$table->boolean('active');
+			$table->string('picture', 1024);
+			$table->json('location');
+			$table->string('address', 512);
+			$table->json('business_hours');
+			$table->json('phones');
+			$table->json('emails');
+			$table->json('contacts');
+			$table->string('website', 1024);
 			$table->timestamps();
+			$table->softDeletes();
+			// Indexes
+			$table->unique(array('account_id', 'name'));
 		});
 	}
 
