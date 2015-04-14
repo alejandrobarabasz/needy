c04ac14dca530a5217a5ebd6bebd38f6669d4d7c
Alejandro Barabasz <alejandro.barabasz@gmail.com>
2015-04-14 09:23:54 -0300

    Added SupportCenter relationships with Account model

--- app/SupportCenter.php
+++ app/SupportCenter.php
@@ -4,6 +4,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class SupportCenter extends Model {
 
-	//
+	// Database table
+	protected $table = 'support_centers';
+
+	/**
+	 * Relationship with parent account
+	 **/
+	public function account() {
+		return $this->belongsTo('NeedFinder\Account', 'account_id');
+	}
 
 }
