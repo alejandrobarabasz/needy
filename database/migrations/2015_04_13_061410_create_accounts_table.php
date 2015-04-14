0f8799d89431bc44972ccf28cf09c55ae029da42
Alejandro Barabasz <alejandro.barabasz@gmail.com>
2015-04-14 09:22:00 -0300

    Added Account model

---
+++ database/migrations/2015_04_13_061410_create_accounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('accounts', function(Blueprint $table)
+		{
+			// Columns
+			$table->bigIncrements('id');
+			$table->string('name', 255);
+			$table->boolean('active');
+			$table->timestamps();
+			$table->timestamp('activated_at');
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('accounts');
+	}
+
+}
