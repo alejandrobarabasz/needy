8c0345e68c103bcaf7d1498a2d0f1a8a2f3d9699
Alejandro Barabasz <alejandro.barabasz@gmail.com>
2015-04-15 23:38:49 -0300

    Added localization messages

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Global Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used by the global application localization
+	|
+	*/
+
+	'form_contains_errors' => 'The form contains errors',
+	'support_center_not_found' => 'Support center not found',
+	'support_center_created_successfully' => 'New support center was created successfully',
+	'support_center_updated_successfully' => 'Support center updated successfully',
+	'support_center_deleted_successfully' => 'Support center deleted successfully',
+
+];
